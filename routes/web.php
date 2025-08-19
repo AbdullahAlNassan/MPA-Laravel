03e73282e9f556d0734ce4bbc3f0c8168c5ecb3e
Abdullah Al Nassan <144023810+AbdullahAlNassan@users.noreply.github.com>
2025-08-19 17:08:01 +0200

    new routes toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show'); // nieuw
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create'); 
+Route::post('/books', [BookController::class, 'store'])->name('books.store');       
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+
 
